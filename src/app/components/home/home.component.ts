da4867ee56e85069a2d824f7c44b30b208da12e2
Angda Song <angda_song@symantec.com>
2019-07-05 17:50:54 -0700

    UI update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -15,4 +15,8 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
+  saveResultsAsCSV(): void {
+
+  }
+
 }
--- src/app/services/uscis.service.ts
+++ src/app/services/uscis.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
 
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { ApiResponse } from '../classes/api-response.class';
+
+import { environment } from '../../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UscisService {
 
   constructor() { }
+
+  
+
 }
